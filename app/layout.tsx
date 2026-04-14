94db97ab7bdd46697acad71785991cfd5b617a36
José Sabino <peixeweb@gmail.com>
2026-04-14 18:19:42 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import { ServiceWorkerRegistration } from "../components/ServiceWorkerRegistration";
 import { InstallPrompt } from "../components/InstallPrompt";
+
 export const metadata = {
   title: "Pesquisa Bíblica",
   description: "Pesquisas Bíblicas Sem Viés. A Verdade da Palavra, com Clareza e Equilíbrio.",
@@ -9,22 +10,23 @@ export const metadata = {
   icons: {
     apple: "/icon-192x192.png",
   },
+}; // <--- ESSA CHAVE AQUI ESTAVA FALTANDO!
 
-  export default function RootLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return (
-      <html lang="pt-BR">
-        <head>
-          <link rel="manifest" href="/manifest.json" />
-        </head>
-        <body>
-          <ServiceWorkerRegistration />
-          <InstallPrompt />
-          {children}
-        </body>
-      </html>
-    );
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="pt-BR">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
+      <body>
+        <ServiceWorkerRegistration />
+        <InstallPrompt />
+        {children}
+      </body>
+    </html>
+  );
 }
